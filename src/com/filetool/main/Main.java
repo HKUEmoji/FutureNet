9bf3e2c7ae47ab83c2f4fc6b4d2f7d00653c46c0
hhtotoro <731009410@qq.com>
2016-03-25 16:04:51 +0800

    graph search can't satisfy the condition.

---
+++ src/com/filetool/main/Main.java
@@ -0,0 +1,50 @@
+package com.filetool.main;
+
+import com.filetool.util.FileUtil;
+import com.filetool.util.LogUtil;
+import com.routesearch.route.Route;
+
+/**
+ * 工具入口
+ * 
+ * @author
+ * @since 2016-3-1
+ * @version v1.0
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+//        if (args.length != 3)
+//        {
+//            System.err.println("please input args: graphFilePath, conditionFilePath, resultFilePath");
+//            return;
+//        }
+//
+//        String graphFilePath = args[0];
+//        String conditionFilePath = args[1];
+//        String resultFilePath = args[2];
+        
+        //test
+    	String [] a = {"./case0/topo.csv","./case0/demand.csv","./case0/result1.csv"};
+        String graphFilePath = a[0];
+        String conditionFilePath = a[1];
+        String resultFilePath = a[2];
+        
+
+        LogUtil.printLog("Begin");
+
+        // 读取输入文件
+        String graphContent = FileUtil.read(graphFilePath, null);
+        String conditionContent = FileUtil.read(conditionFilePath, null);
+
+        // 功能实现入口
+        String resultStr = Route.searchRoute(graphContent, conditionContent);
+
+        // 写入输出文件
+        FileUtil.write(resultFilePath, resultStr, false);
+
+        LogUtil.printLog("End");
+    }
+
+}
